5d3abae2212ad40e1b53d0345f45340549cbd4f9
Mevan Samaratunga <mevansam@gmail.com>
2017-01-08 14:19:58 +0400

    creates a role to allow evaluating dynamic expressions as well as a database user for the catalog-web application

---
+++ scripts/mongodb/create_user.js
@@ -0,0 +1,62 @@
+// This script needs to be executed as the database admin
+
+// Create role in "admin" database with execute privileges on any function
+// https://www.claudiokuenzler.com/blog/555/allow-mongodb-user-execute-command-eval-mongodb-3.x#.WHHxBrZ97MU
+
+use admin
+
+if (db.system.users.find({user: 'root'}).count()==0) { 
+    db.createUser( {
+        user: "root",
+        pwd: "sentilo",
+        roles: [ {
+            "role" : "readWriteAnyDatabase",
+            "db" : "admin"
+        }, {
+            "role" : "userAdminAnyDatabase",
+            "db" : "admin"
+        }, {
+            "role" : "dbAdminAnyDatabase",
+            "db" : "admin"
+        }, {
+            "role" : "clusterAdmin",
+            "db" : "admin"
+        } ]
+    } )
+}
+if (db.system.roles.find({role: 'executeFunctions'}).count()==0) { 
+    db.createRole( { 
+        role: "executeFunctions", 
+        privileges: [ { 
+            resource: { 
+                anyResource: true 
+            }, 
+            actions: [ 
+                "anyAction" 
+            ] 
+        } ], 
+        roles: [] 
+    } );
+}
+
+// Create user in a database named "sentilo"
+
+use sentilo
+
+if (db.system.users.find({user: 'sentilo'}).count()==0) { 
+    db.createUser( { 
+        user: "sentilo",
+        pwd: "sentilo",
+        roles: [ { 
+            role: "dbAdmin", db: "sentilo" 
+        }, { 
+            role: "dbOwner", db: "sentilo" 
+        }, { 
+            role: "readWrite", db: "sentilo" 
+        }, { 
+            role: "executeFunctions", db: "admin" 
+        } ] 
+    } )
+} else {
+    db.grantRolesToUser("sentilo", [ { role: "executeFunctions", db: "admin" } ])
+}
